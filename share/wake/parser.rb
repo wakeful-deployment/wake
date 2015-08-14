192641f2b1af2f254cd1afdef26c671f6a003b74
Ryan Levick <ryan@6wunderkinder.com>
2015-08-14 17:42:51 +0200

    Allow subcommands

--- share/wake/parser.rb
+++ share/wake/parser.rb
@@ -11,9 +11,14 @@ class OptsParser
     @required = []
     @optional = []
     @boolean  = []
+    @subcommands = []
     @options  = {}
     @parser   = OptionParser.new
     yield self
+  end
+
+  def subcommand(name, file)
+    @subcommands << [name, file]
   end
 
   def banner=(banner)
@@ -64,6 +69,19 @@ class OptsParser
   end
 
   def parse!
+    if subcommand = fetch_subcommand
+      execute_subcommand(subcommand)
+    end
+
+    if @received.none? && @subcommands.any?
+      subcommand_names = @subcommands.map{|sc| sc.first }
+      panic! "missing required subcommands: #{subcommand_names.join(",")}"
+    end
+
+    perform_top_level_parse!
+  end
+
+  def perform_top_level_parse!
     @parser.parse!
 
     missing_keys = @required - @received
@@ -77,6 +95,21 @@ class OptsParser
     end
   rescue OptionParser::MissingArgument, OptionParser::InvalidOption
     panic! "#{$!}\n#{@parser}"
+  end
+
+  def execute_subcommand(command)
+    file = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "libexec", command))
+    cmd = "#{file} #{ARGV.drop(1).join(" ")}"
+
+    exec cmd
+    exit(0)
+  end
+
+  def fetch_subcommand
+    name, file = (name = ARGV.first) &&
+      !name.match(/^-/) &&
+      @subcommands.find{|sc| sc.first == name.to_sym}
+    file
   end
 
   def validate_short_name(short_name)
