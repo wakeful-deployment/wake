ddc366ce9907961bb1ac88c89882ce074ccda109
myobie <me@nathanherald.com>
2015-10-12 12:43:57 +0200

    Remove the old way of doing things

--- platforms/golang.sh
+++
@@ -1,6 +0,0 @@
-. /opt/assets/scripts/logger.sh
-. /opt/assets/scripts/consul.sh
-. /opt/assets/scripts/statsite.sh
-. /opt/assets/scripts/open_ssl.sh
-. /opt/assets/scripts/ruby.sh
-. /opt/assets/scripts/connecty.sh
--- scripts/statsite.sh
+++
@@ -1,1 +0,0 @@
-curl -s https://s3-eu-west-1.amazonaws.com/wake/blobs/statsite -o /opt/bin/statsite
