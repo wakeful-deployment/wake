9d7546fe5790a909781538dde11c9f77a36ca583
myobie <me@nathanherald.com>
2015-10-14 17:18:59 +0200

    wake-pack works for statsite

--- share/wake/run.rb
+++ share/wake/run.rb
@@ -3,16 +3,16 @@ require_relative 'panic'
 
 module Kernel
   def run(cmd, verbose = false)
-    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+    Open3.popen3(cmd) do |i, o, e, t|
       out, err = [[], []]
-      while (stdout = stdout.gets) || (stderr = stderr.gets)
+      while (stdout = o.gets) || (stderr = e.gets)
         puts stdout if verbose && stdout
         puts stderr if verbose && stderr
         out << stdout if stdout
         err << stderr if stderr
       end
 
-      exit_code = wait_thr.value
+      exit_code = t.value
       [out, err, exit_code]
     end
   end
