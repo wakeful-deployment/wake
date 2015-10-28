007bd148eb7c5ddcb7e1f0050b8e3c4db5d88723
Donovan Lange <dolange@microsoft.com>
2015-10-28 11:33:51 -0700

    Columnize text in ruby (native)

--- share/wake/text_utils.rb
+++ share/wake/text_utils.rb
@@ -4,15 +4,10 @@ module TextUtils
   module_function
 
   def columnize(text)
-    Open3.popen3("column -t") do |stdin, stdout, stderr, wait_thr|
-      stdin.puts text
-      stdin.close
-
-      if wait_thr.value.success?
-        stdout.read.chomp
-      else
-        text
-      end
+    columnBoundary = text.map{|line| line.split[0].length}.max
+    text.map do |line|
+      tokens = line.split 
+      "%-#{columnBoundary}s %s\n" %[tokens[0], tokens[1]]
     end
   end
 
