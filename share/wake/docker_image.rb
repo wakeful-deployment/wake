256dd022f54cbf56176741f499c9a28b688e0de0
myobie <me@nathanherald.com>
2015-11-17 15:55:15 +0100

    Using --build-arg now instead of erb

--- share/wake/docker_image.rb
+++ share/wake/docker_image.rb
@@ -1,28 +1,39 @@
 class DockerImage
-  def initialize(org:, app:, process:, rev:)
+  def initialize(org:, app:, process:, rev:, release: false)
     @org = org
     @app = app
     @process = process
     @rev = rev
+    @release = release
+  end
+
+  def unique_name
+    if @app == @process
+      @app
+    else
+      "#{@app}-#{process}"
+    end
   end
 
   def name
-    name = if @app == @process
-             @app
-           else
-             "#{@app}-#{process}"
-           end
+    "#{@org}/wake-#{unique_name}:#{@rev}"
+  end
 
-    "#{@org}/wake-#{name}:#{@rev}"
+  def release_name
+    "#{@org}/wake-#{unique_name}-release:#{@rev}"
   end
 
   def to_hash
-    {
+    hash = {
       org: @org,
       app: @app,
       process: @process,
       rev: @rev,
       name: name
     }
+
+    hash.merge!(release_name: release_name) if @release
+
+    hash
   end
 end
