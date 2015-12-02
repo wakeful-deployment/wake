222ca61d8241dcae2e435fb4ad2842bf038ce52f
nathan-ms <nathan.herald@microsoft.com>
2015-12-02 18:41:34 +0100

    New docker opts and port poller

---
+++ share/wake/azure/port_poller.rb
@@ -0,0 +1,33 @@
+module Wake
+  module Azure
+    class PortPoller
+      attr_reader :ip, :port, :tries, :max_tries
+
+      def new(ip:, port:, max_tries: 100)
+        @ip        = ip
+        @port      = port
+        @tries     = 0
+        @max_tries = max_tries
+      end
+
+      def call
+        loop do
+          `nc -z -w5 #{ip} #{port}`
+
+          if $?.success?
+            break
+          elsif tries >= max_tries
+            fail "port #{port} never became available"
+          else
+            tries += 1
+            sleep 3
+          end
+        end
+      end
+
+      def self.call(**opts)
+        new(**opts).call
+      end
+    end
+  end
+end
