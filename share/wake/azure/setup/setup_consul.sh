f59f3fee28cd2744ae649659f1550519a15d89c0
nathan-ms <nathan.herald@microsoft.com>
2015-12-23 15:15:18 +0100

    Install jq and use it to pre-pull docker images presentin operator.json

--- share/wake/azure/setup/setup_consul.sh
+++ share/wake/azure/setup/setup_consul.sh
@@ -6,3 +6,11 @@ set -x
 mv operator.json /opt/config/operator.json
 chown root:root -R /opt/config
 chmod 644 -R /opt/config
+
+# pre-pull all known global images
+
+$images=$(jq -a -r ".boot_images | map(.image) | .[]" /opt/config/operator.json)
+
+for image in $images; do
+  docker pull $image
+done
